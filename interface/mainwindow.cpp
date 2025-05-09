9bafaffb3d2502d7f0b1ec88ba68464f755b65c3
Abraham Venegas Mayorga <abravenegas@estudiantec.cr>
2025-05-08 22:13:22 -0600

    Interface update
    -Graphic's buttons added
    -File management implemented, it needs to be adjusted to the logic

--- interface/mainwindow.cpp
+++ interface/mainwindow.cpp
@@ -1,14 +1,226 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_uploadButton1_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton2_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton3_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton4_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+void MainWindow::on_uploadButton5_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton6_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton7_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_uploadButton8_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Select a File", "", "Text Files (*.txt);;All Files (*)");
+
+    if (!fileName.isEmpty()) {
+        QMessageBox::information(this, "File Selected", "You selected:\n" + fileName);
+    }
+}
+
+void MainWindow::on_PEButton1_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton1->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton2_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton2->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton3_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton3->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton4_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton4->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton5_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton5->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton6_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton6->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton7_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton7->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_PEButton8_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->PEButton8->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_ICButton_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->ICButton->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_SMButton_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->SMButton->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_GRAPHICButton_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->GRAPHICButton->setStyleSheet("background-color: green; color: white;");
+
+}
+void MainWindow::on_GRAPHICButton2_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->GRAPHICButton2->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton2_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton2->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton3_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton3->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton4_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton4->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton5_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton5->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton6_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton6->setStyleSheet("background-color: green; color: white;");
+
+}
+void MainWindow::on_LSButton7_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton7->setStyleSheet("background-color: green; color: white;");
+
+}
+
+void MainWindow::on_LSButton8_clicked()
+{
+        // Cambiar color del botón a verde
+        ui->LSButton->setStyleSheet("background-color: green; color: white;");
+
 }
--- interface/mainwindow.h
+++ interface/mainwindow.h
@@ -2,20 +2,75 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_uploadButton2_clicked();
+
+    void on_uploadButton3_clicked();
+
+    void on_uploadButton4_clicked();
+
+    void on_uploadButton5_clicked();
+
+    void on_uploadButton6_clicked();
+
+    void on_uploadButton7_clicked();
+
+    void on_uploadButton8_clicked();
+
+    void on_uploadButton1_clicked();
+
+    void on_PEButton1_clicked();
+
+    void on_PEButton2_clicked();
+
+    void on_PEButton3_clicked();
+
+    void on_PEButton4_clicked();
+
+    void on_PEButton5_clicked();
+
+    void on_PEButton6_clicked();
+
+    void on_PEButton7_clicked();
+
+    void on_PEButton8_clicked();
+
+    void on_ICButton_clicked();
+
+    void on_SMButton_clicked();
+
+    void on_GRAPHICButton_clicked();
+
+    void on_GRAPHICButton2_clicked();
+
+    void on_LSButton_clicked();
+
+    void on_LSButton2_clicked();
+
+    void on_LSButton3_clicked();
+
+    void on_LSButton4_clicked();
+
+    void on_LSButton5_clicked();
+
+    void on_LSButton6_clicked();
+
+    void on_LSButton7_clicked();
+
+    void on_LSButton8_clicked();
 
 private:
     Ui::MainWindow *ui;
